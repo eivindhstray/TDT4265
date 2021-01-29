7eb242e5ac2eb3465d378c575abc648fa3e6ea4f
fridthoy <hoyer1996@hotmail.com>
2021-01-29 12:00:33 +0100

    adding model 4c

--- assignment1/task4b.py
+++ assignment1/task4b.py
@@ -2,6 +2,11 @@ import numpy as np
 import utils
 import mnist
 from task2a import pre_process_images
+from trainer import BaseTrainer
+from task2 import LogisticTrainer
+from task2a import BinaryModel
+from task3a import SoftmaxModel
+from task3 import SoftmaxTrainer, cross_entropy_loss, one_hot_encode, calculate_accuracy
 
 np.random.seed(1)
 from tqdm import tqdm
@@ -58,5 +63,45 @@ def visualModeltraining():
     plt.imsave("task4b_softmax_weight.png", frame, cmap="gray")
     return frame
 
+def makeAccimage():
+    # hyperparameters DO NOT CHANGE IF NOT SPECIFIED IN ASSIGNMENT TEXT
+    num_epochs = 2
+    learning_rate = 0.01
+    batch_size = 128
+    l2_reg_lambda = 0.01
+    shuffle_dataset = True
+
+    # Load dataset
+    X_train, Y_train, X_val, Y_val = utils.load_full_mnist()
+    X_train = pre_process_images(X_train)
+    X_val = pre_process_images(X_val)
+    Y_train = one_hot_encode(Y_train, 10)
+    Y_val = one_hot_encode(Y_val, 10)
+
+    # ANY PARTS OF THE CODE BELOW THIS CAN BE CHANGED.
+
+    # Intialize model
+
+    #plotting for different lambdas:
+    l2_lambdas = [1, .1, .01, .001]
+
+    for i in l2_lambdas:
+        model = SoftmaxModel(l2_reg_lambda=i)
+        # Train model
+        trainer = SoftmaxTrainer(
+            model, learning_rate, batch_size, shuffle_dataset,
+            X_train, Y_train, X_val, Y_val,
+        )
+        train_history, val_history = trainer.train(num_epochs)
+
+        # Plot accuracy
+        plt.ylim([0.60, .95])
+        utils.plot_loss(val_history["accuracy"], "Validation Accuracy with $\lambda$ ={}".format(i))
+
+    plt.xlabel("Number of Training Steps")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.show()
+
 if __name__ == '__main__':
-    visualModeltraining()
+    makeAccimage()
