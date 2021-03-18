42b5332c31435182c6f3b4eca1f4b679d64879cc
Eivind <eivindhstray@gmail.com>
2021-03-18 11:22:33 +0100

    update

--- assignment4/SSD/ssd/modeling/backbone/improved.py
+++ assignment4/SSD/ssd/modeling/backbone/improved.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 
 
-class BasicModel(torch.nn.Module):
+class ImprovedModel(torch.nn.Module):
     """
     This is a basic backbone for SSD.
     The feature extractor outputs a list of 6 feature maps, with the sizes:
